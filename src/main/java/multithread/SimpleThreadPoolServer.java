d09250cac450f082fad1900632730c5e9dc59fd7
ybyao <ybyao@iflytek.com>
2019-10-25 18:01:52 +0800

    2019年10月25日18:01:45

---
+++ src/main/java/multithread/SimpleThreadPoolServer.java
@@ -0,0 +1,14 @@
+package multithread;
+
+import sun.nio.ch.ThreadPool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author : ybyao
+ * @Create : 2019-10-25 16:08
+ */
+public class SimpleThreadPoolServer {
+
+//    ThreadPool pool=new ThreadPool();
+}
